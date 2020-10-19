a9977597d5c0c8dce11dc753957408bf34c172bd
rizwanrasheed111333 <72903579+rizwanrasheed111333@users.noreply.github.com>
2020-10-19 18:26:20 +0500

    Add files via upload

---
+++ CustomerController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mvccrud.Models;
+namespace mvccrud.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            using (DbModels dbmodel = new DbModels())
+            {
+                return View(dbmodel.Customers.ToList());
+            }
+        }
+
+        // GET: Customer/Details/5
+        public ActionResult Details(int id)
+        {
+            using (DbModels dbmodel = new DbModels())
+            {
+                return View(dbmodel.Customers.Where(x=>x.Id==id).FirstOrDefault());
+            }
+        }
+
+        // GET: Customer/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customer/Create
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                using (DbModels dbmodel = new DbModels())
+                {
+                    dbmodel.Customers.Add(customer);
+                    dbmodel.SaveChanges();
+                }
+
+                    return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Customer/Edit/5
+        public ActionResult Edit(int id)
+        {
+            using (DbModels dbmodel = new DbModels())
+            {
+                return View(dbmodel.Customers.Where(x => x.Id == id).FirstOrDefault());
+            }
+        }
+
+        // POST: Customer/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Customer customer)
+        {
+            try
+            {
+                using (DbModels dbmodel = new DbModels())
+                {
+                    dbmodel.Entry(customer).State = EntityState.Modified;
+                    dbmodel.SaveChanges();
+                }
+                    // TODO: Add update logic here
+
+                    return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Customer/Delete/5
+        public ActionResult Delete(int id)
+        {
+            using (DbModels dbmodel = new DbModels())
+            {
+                return View(dbmodel.Customers.Where(x => x.Id == id).FirstOrDefault());
+            }
+        }
+
+        // POST: Customer/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                using(DbModels dbModel=new DbModels())
+                {
+                    Customer customer = dbModel.Customers.Where(x => x.Id == id).FirstOrDefault();
+                    dbModel.Customers.Remove(customer);
+                    dbModel.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
